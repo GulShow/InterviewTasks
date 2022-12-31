9f8e6a926e920bc16bedcdb814c6e31d8a4867f9
Stanislav Huliaiev <101598952+GulShow@users.noreply.github.com>
2022-12-31 16:45:24 +0200

    first commit

--- js/task1.js
+++ js/task1.js
@@ -1,29 +1,17 @@
 'use strict';
 
-const main = () => {
-    const number = prompt('Введите число:');
-    const arr = number.split('');
-    if(isNaN(+number)){
-        alert('Введите число!');
-        return;
-    }
-    if(arr[arr.length - 1] === '1'){
-        alert(`Вам ${number} год`);
-        return;
-    }
-
-    if(arr[arr.length - 1] >= '5'){
-        alert(`Вам ${number} лет`);
-        return;
-    }
-
-    if(arr[arr.length - 1] < '5'){
-        alert(`Вам ${number} года`);
-    }
-
+const fibonacci = (num) => {
+   if(num <= 1) return num;
+   else return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-main()
+const memo = f => {
+    const cache = {};
+    return arg => cache[arg] || (cache[arg] = f(arg))
+}
+
+const result = memo(fibonacci);
+console.log(result(16));
 
 
 
